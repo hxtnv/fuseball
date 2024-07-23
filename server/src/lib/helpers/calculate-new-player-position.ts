0a305171f8647a47d11a9d8d572f048a5f79267e
theneuetimes <wygonski@gmail.com>
2024-07-23 20:08:19 +0200

    improve collision

--- server/src/lib/helpers/calculate-new-player-position.ts
+++ server/src/lib/helpers/calculate-new-player-position.ts
@@ -31,35 +31,53 @@ const calculateNewPlayerPosition = ({
     newPosition.x += PLAYER.SPEED;
   }
 
-  // Goal zone dimensions
-  const goalZoneWidth =
-    MAP.FIELD_WIDTH * MAP.GOAL_ZONE_WIDTH_RATIO - PLAYER.SIZE / 2;
+  // limit movement to the map + goal zones
+  const goalZoneWidth = MAP.FIELD_WIDTH * MAP.GOAL_ZONE_WIDTH_RATIO;
   const goalZoneHeight = MAP.FIELD_HEIGHT * MAP.GOAL_ZONE_HEIGHT_RATIO;
 
   const centerY = MAP.FIELD_HEIGHT / 2;
   const goalZoneTopY = centerY - goalZoneHeight / 2;
   const goalZoneBottomY = centerY + goalZoneHeight / 2;
 
-  // Set up bounds
+  const playerSizeHalf = PLAYER.SIZE / 2;
+
+  // we are in the goal zone (vertically)
   if (
-    (newPosition.x < goalZoneWidth &&
-      newPosition.y >= goalZoneTopY &&
-      newPosition.y <= goalZoneBottomY) ||
-    (newPosition.x > MAP.FIELD_WIDTH - goalZoneWidth &&
-      newPosition.y >= goalZoneTopY &&
-      newPosition.y <= goalZoneBottomY)
+    newPosition.y > goalZoneTopY - playerSizeHalf &&
+    newPosition.y < goalZoneBottomY + playerSizeHalf
   ) {
-    // Allow movement in the goal zones
-    newPosition.x = Math.max(
-      -goalZoneWidth,
-      Math.min(MAP.FIELD_WIDTH + goalZoneWidth, newPosition.x)
-    );
-    newPosition.y = Math.max(
-      PLAYER.SIZE / 2,
-      Math.min(MAP.FIELD_HEIGHT - PLAYER.SIZE / 2, newPosition.y)
-    );
+    const boundsLeft = -goalZoneWidth + playerSizeHalf;
+    const boundsRight = MAP.FIELD_WIDTH + goalZoneWidth - playerSizeHalf;
+
+    // we are outside the LEFT goal zone (horizontally)
+    if (newPosition.x < boundsLeft) {
+      newPosition.x = boundsLeft;
+    }
+
+    // we are outside the RIGHT goal zone (horizontally)
+    if (newPosition.x > boundsRight) {
+      newPosition.x = boundsRight;
+    }
+
+    // we are inside of the LEFT goal zone (horizontally)
+    const isInsideLeftGoalZone =
+      newPosition.x >= boundsLeft && newPosition.x <= 0;
+
+    const isInsideRightGoalZone =
+      newPosition.x >= MAP.FIELD_WIDTH && newPosition.x <= boundsRight;
+
+    if (isInsideLeftGoalZone || isInsideRightGoalZone) {
+      // if we go too far up
+      if (newPosition.y < goalZoneTopY + playerSizeHalf) {
+        newPosition.y = goalZoneTopY + playerSizeHalf;
+      }
+
+      // if we go too far down
+      if (newPosition.y > goalZoneBottomY - playerSizeHalf) {
+        newPosition.y = goalZoneBottomY - playerSizeHalf;
+      }
+    }
   } else {
-    // Normal bounds for the main field
     newPosition.x = Math.max(
       PLAYER.SIZE / 2,
       Math.min(MAP.FIELD_WIDTH - PLAYER.SIZE / 2, newPosition.x)
@@ -69,24 +87,6 @@ const calculateNewPlayerPosition = ({
       Math.min(MAP.FIELD_HEIGHT - PLAYER.SIZE / 2, newPosition.y)
     );
   }
-
-  // Don't collide with other players
-  // Adjust collision logic to account for both main field and goal zones
-  /*
-  allPlayers.forEach((otherPlayer) => {
-    if (otherPlayer.id !== player.id) {
-      if (
-        otherPlayer.position.x < newPosition.x + PLAYER.SIZE / 2 &&
-        otherPlayer.position.x + PLAYER.SIZE / 2 > newPosition.x &&
-        otherPlayer.position.y < newPosition.y + PLAYER.SIZE / 2 &&
-        otherPlayer.position.y + PLAYER.SIZE / 2 > newPosition.y
-      ) {
-        // Correct the position to avoid collision
-        newPosition.x = player.position.x;
-        newPosition.y = player.position.y;
-      }
-    }
-  });*/
 
   return newPosition;
 };

ba64bdff4e6966a1ea4921dbf0ab278cc6300c31
theneuetimes <wygonski@gmail.com>
2024-08-01 15:21:46 +0200

    add loading state to player preview

--- client/src/components/domain/player-preview/index.tsx
+++ client/src/components/domain/player-preview/index.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "react";
 import Lock from "@/assets/icons/lock-solid.svg?react";
 import Ribbon from "@/assets/ribbon.svg?react";
 import Button from "@/components/common/button";
+import Skeleton from "@/components/common/skeleton";
 import { useWebSocket } from "@/context/websocket.context";
 import useModal from "@/hooks/use-modal/use-modal";
 
@@ -26,8 +27,6 @@ const PlayerPreview: React.FC = () => {
   const { playerData } = useWebSocket();
   const { open, Modal } = useModal();
 
-  if (!playerData) return null;
-
   return (
     <Fragment>
       <Modal title="Sign in">
@@ -42,18 +41,36 @@ const PlayerPreview: React.FC = () => {
         </div>
 
         <div className={styles.preview__content}>
-          <label>Playing as</label>
-          <h4>{playerData.name}</h4>
+          {!playerData ? (
+            <Fragment>
+              <label>Playing as</label>
+              <Skeleton style={{ height: "16px", width: "110px" }} />
 
-          <div className={styles.preview__content__stats}>
-            <StatBox title="Total wins" value={5} locked={true} />
-            <StatBox title="Total goals" value={100} locked={true} />
-            <StatBox title="Win rate" value={"78%"} locked={true} />
-          </div>
+              <div className={styles.preview__content__stats}>
+                {["Total wins", "Total goals", "Win rate"].map((title, key) => (
+                  <div key={key}>
+                    <label>{title}</label>
+                    <Skeleton style={{ height: "16px", width: "32px" }} />
+                  </div>
+                ))}
+              </div>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <label>Playing as</label>
+              <h4>{playerData.name}</h4>
 
-          <Button variant="primary" size="small" onClick={open}>
-            Sign in to unlock stats
-          </Button>
+              <div className={styles.preview__content__stats}>
+                <StatBox title="Total wins" value={5} locked={true} />
+                <StatBox title="Total goals" value={100} locked={true} />
+                <StatBox title="Win rate" value={"78%"} locked={true} />
+              </div>
+
+              <Button variant="primary" size="small" onClick={open}>
+                Sign in to unlock stats
+              </Button>
+            </Fragment>
+          )}
         </div>
       </div>
     </Fragment>
